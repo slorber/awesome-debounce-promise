d850eb67ae7eee2d9b4756476e719f052727c72d
slorber <lorber.sebastien@gmail.com>
2019-02-26 10:43:41 +0100

    better impl + some fixes

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,17 @@ import { onlyResolvesLast } from 'awesome-only-resolves-last-promise';
 
 type ArgumentsType<T> = T extends (...args: infer A) => any ? A : never;
 
-// We use DebouncePromise as a dependency as it does a great low-level job
-// The behavior of the lib is to return the same promise for all function calls
-export const debouncePromise = DebouncePromise;
-
 export type AwesomeDebounceOptions = {
-  key: (...args: any[]) => string;
+  key: (...args: any[]) => string | null | undefined;
   onlyResolvesLast: boolean;
 } & DebouncePromise.DebounceOptions;
 
-const DefaultKey = 'DEFAULT_AWESOME_DEBOUNCE_PROMISE_KEY';
 
 const DefaultOptions = {
   // One distinct debounced function is created per key and added to an internal cache
   // By default, the key is null, which means that all the calls
   // will share the same debounced function
-  key: (..._args: any[]) => DefaultKey,
+  key: (..._args: any[]) => null,
 
   // By default, a debounced function will only resolve
   // the last promise it returned
@@ -28,58 +23,96 @@ const DefaultOptions = {
   onlyResolvesLast: true,
 };
 
+
+type CacheConfig<Fun extends (...args: any[]) => any> = {
+  func: Fun,
+  wait: number,
+  options: AwesomeDebounceOptions
+}
+
+// TODO not useful right now but will be useful if we want to support cancellation
+type CacheFunction<Fun extends (...args: any[]) => any> = {
+  func: Fun,
+  // cancel: () => void,
+}
+
 // We create a debouncing function cache, because when wrapping the original function,
 // we may actually want to route the function call to different debounced functions depending function paameters
-export class DebounceCache {
-  debounceCache: object;
+class DebounceCache<Fun extends (...args: any[]) => any> {
+  config: CacheConfig<Fun>;
 
-  constructor() {
-    this.debounceCache = {};
+  debounceSingleton: CacheFunction<Fun> | null; // key not used
+  debounceCache: { [key: string]: CacheFunction<Fun> }; // key used
+
+  constructor(config: CacheConfig<Fun>) {
+    this.config = config;
+    this.debounceSingleton = null;
+    this.debounceCache = {}; // when key feature is used
   }
 
-  getDebouncedFunction<T extends (...args: any[]) => any>(
-    func: T,
-    wait: number,
-    options: AwesomeDebounceOptions,
-    args: ArgumentsType<T>,
-  ) {
-    const {
-      key: keyOptions,
-      onlyResolvesLast: onlyResolvesLastOption,
-      ...otherOptions
-    } = options;
-    const key = keyOptions(...args);
-    // If the debounced function does not exist for this key, we create one on the fly and return it
-    if (!this.debounceCache[key]) {
-      let debouncedFunc = debouncePromise(func, wait, otherOptions);
-      if (onlyResolvesLastOption) {
-        debouncedFunc = onlyResolvesLast(debouncedFunc as any); // TODO fix TS
-      }
-      this.debounceCache[key] = debouncedFunc;
+  _createDebouncedFunction(): CacheFunction<Fun> {
+    let debouncedFunc: Fun = DebouncePromise(this.config.func, this.config.wait, this.config.options) as any; // TODO TS
+    if (this.config.options.onlyResolvesLast) {
+      debouncedFunc = onlyResolvesLast(debouncedFunc);
     }
-    return this.debounceCache[key];
+    return {
+      func: debouncedFunc,
+    };
+  };
+
+  getDebouncedFunction(
+    args: ArgumentsType<Fun>,
+  ): CacheFunction<Fun> {
+    const key = this.config.options.key(...args);
+    if (key === null || typeof key === 'undefined') {
+      if (!this.debounceSingleton) {
+        this.debounceSingleton = this._createDebouncedFunction();
+      }
+      return this.debounceSingleton;
+    }
+    else {
+      if (!this.debounceCache[key]) {
+        this.debounceCache[key] = this._createDebouncedFunction();
+      }
+      return this.debounceCache[key];
+    }
   }
 }
 
-function AwesomeDebouncePromise<T extends (...args: any[]) => any>(
-  func: T,
+// extra methods are "added" to the the returned debounced async function
+type ReturnedFunction<Fun> = Fun & {
+  // cancel: (key?: string) => void
+}
+
+function AwesomeDebouncePromise<Fun extends (...args: any[]) => any>(
+  func: Fun,
   wait: number,
   options?: Partial<AwesomeDebounceOptions>,
-) {
+): ReturnedFunction<Fun> {
   const finalOptions: AwesomeDebounceOptions = {
     ...DefaultOptions,
     ...options,
   };
-  const debounceCache = new DebounceCache();
-  return function AwesomeDebouncePromiseWrapper(...args: ArgumentsType<T>) {
+
+  const debounceCache = new DebounceCache<Fun>({
+    func,
+    wait,
+    options: finalOptions,
+  });
+
+  const AwesomeDebouncePromiseWrapper = ((...args: ArgumentsType<Fun>) => {
     const debouncedFn = debounceCache.getDebouncedFunction(
-      func,
-      wait,
-      finalOptions,
       args,
-    );
+    ).func;
     return debouncedFn(...args);
+  }) as ReturnedFunction<Fun>; // TODO fix TS
+
+  /*
+  AwesomeDebouncePromiseWrapper.cancel = (key?: string) => {
+
   };
+  */
+  return AwesomeDebouncePromiseWrapper;
 }
 
 export default AwesomeDebouncePromise;
